f0314d8302775aa0a7dc07038e94063e6bcc7b38
Shahram Chaudhry <sc9425@nyu.edu>
2024-12-28 15:11:09 +0500

    vercel deployable?

--- config.mjs
+++
@@ -1,7 +0,0 @@
-
-import { config } from 'dotenv';
-
-config(); 
-export const DSN = process.env.DSN;
-export const SESSION_SECRET = process.env.SESSION_SECRET;
-export const PORT = process.env.PORT ?? 3000;
